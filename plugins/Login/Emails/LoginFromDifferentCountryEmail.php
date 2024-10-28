d929aab9c31676c6f41e9e3d2a5cb1851042fce8
Marc Neudert <marc@innocraft.com>
2024-10-28 16:09:43 +0100

    Allow full email extend/override

--- plugins/Login/Emails/LoginFromDifferentCountryEmail.php
+++ plugins/Login/Emails/LoginFromDifferentCountryEmail.php
@@ -22,23 +22,22 @@ class LoginFromDifferentCountryEmail extends Mail
     /**
      * @var string
      */
-    private $login;
+    protected $login;
 
     /**
      * @var string
      */
-    private $email;
+    protected $email;
 
     /**
      * @var string
      */
-    private $country;
+    protected $country;
 
     /**
      * @var string
      */
-    private $ip;
-
+    protected $ip;
 
     public function __construct(string $login, string $email, string $country, string $ip)
     {
@@ -61,24 +60,24 @@ class LoginFromDifferentCountryEmail extends Mail
         $this->setWrappedHtmlBody($this->getDefaultBodyView());
     }
 
-    private function getDefaultSubject(): string
+    protected function getDefaultSubject(): string
     {
         return Piwik::translate('Login_LoginFromDifferentCountryEmailSubject');
     }
 
-    private function getDateAndTimeFormatted(): string
+    protected function getDateAndTimeFormatted(): string
     {
         return Date::now()->getLocalized(DateTimeFormatProvider::DATETIME_FORMAT_LONG);
     }
 
-    private function getPasswordResetLink(): string
+    protected function getPasswordResetLink(): string
     {
         return Url::getCurrentUrlWithoutQueryString()
             . '?module=' . Piwik::getLoginPluginName()
             . '&showResetForm=1';
     }
 
-    private function getEnable2FALink(): string
+    protected function getEnable2FALink(): string
     {
         if (PluginManager::getInstance()->isPluginActivated('TwoFactorAuth')) {
             return Url::getCurrentUrlWithoutQueryString() . '?module=UsersManager&action=userSecurity';
@@ -87,7 +86,7 @@ class LoginFromDifferentCountryEmail extends Mail
         }
     }
 
-    private function getDefaultBodyView(): string
+    protected function getDefaultBodyView(): View
     {
         $view = new View('@Login/_loginFromDifferentCountryEmail.twig');
         $view->login = $this->login;
@@ -97,6 +96,6 @@ class LoginFromDifferentCountryEmail extends Mail
         $view->resetPasswordLink = $this->getPasswordResetLink();
         $view->enable2FALink = $this->getEnable2FALink();
 
-        return $view->render();
+        return $view;
     }
 }
